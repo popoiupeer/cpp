05013692fa247528bb78e37215de3b1e41704bb3
popoiupeer <erdand160307@gmail.com>
2024-10-29 21:20:59 +0700

    Create hw29.10.24.cpp

---
+++ hw29.10.24.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    /*
+    задание 9
+    -double a, b, c;
+     cout << "введите сопротивления через пробел"<< endl;
+     cin >> a >> b >> c;
+     cout << "сопротивление равно:" << a + b + c;
+ 
+
+    задание24 
+    int x1=0, x2=0, y1=0, y2=0;
+    cout << "введите x1"<<endl;
+    cin >> x1;
+    cout << "введите x2" << endl;
+    cin >> x2;
+    cout << "введите y1" << endl;
+    cin >> y1;
+    cout << "введите y2" << endl;
+    cin >> y2;
+    cout << sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    
+    задание 67 
+    int a;
+    cout << "введите число от 1 до 100 " << endl;
+    cin >> a;
+    if (a < 10) {
+        cout << "a)1 цифра " << endl;
+    }
+    if ((a>=10)&&(a<100))
+    { cout << "а)2 цифры" << endl;
+    }
+    if (a == 100) {
+        cout << "а)3 цифры"<< endl  ;
+    }
+    cout << "б)" << a % 10 + a / 10 << endl;
+
+    cout << "в)" << a % 10 << endl;
+    cout << "г)" << a / 10;
+
+    задание 36
+    double a, b, c;
+    cout << "введите числа a,b,c через пробел" << endl;
+    cin >> a>>b>> c;
+    if ((a < b )&&( b < c)) {
+        cout <<  "выражение действительно " << endl;
+    }
+    else { cout << "выражение не действительно" << endl; }
+    
+    
+    задание 41
+    double a, b, c;
+    cout << "введите три числа через пробел"<<endl;
+    cin >> a >> b >> c;
+    if ((a <= 3) && (a >= 1)) { cout << a;
+    }
+    if ((b <= 3) && (b >= 1)) {
+        cout << b;
+    }
+    if ((c <= 3) && (c >= 1)) {
+        cout << c;
+    }*/
+}
