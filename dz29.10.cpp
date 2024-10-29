671e8a00e0fd343e167c436a5f0ddd3723041595
popoiupeer <erdand160307@gmail.com>
2024-10-29 11:37:42 +0700

    up dz29.10.cpp

--- dz29.10.cpp
+++ dz29.10.cpp
@@ -1,9 +1,15 @@
-double x, y;
-cout << "введите х";
-cin >> x;
-cout << "введите y";
-cin >> y;
-if (0.5 < x < 0.5 ) { cout << "+"; }
-else { cout << "-"; }
-if (1 < y < 1) { cout << "+"; }
-else { cout << "-"; }
+#include <iostream>;
+#include "math.h"
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    double x, y;
+    cout << "введите х: ";
+    cin >> x;
+    cout << "введите y: ";
+    cin >> y;
+
+    if ((y <= 0, 5 * x + 1) && (y >= -0, 5 * x - 1) or ((1 >= pow(y, 2) + pow(x, 2)) and x >= 0))
+    {cout << "входит ";}
+    else { cout << "не входит "; }
