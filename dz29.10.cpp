4cbda969485865a450f3e382b3ea3785701c544a
popoiupeer <erdand160307@gmail.com>
2024-10-29 09:43:56 +0700

    Create dz29.10.cpp

---
+++ dz29.10.cpp
@@ -0,0 +1,9 @@
+double x, y;
+cout << "введите х";
+cin >> x;
+cout << "введите y";
+cin >> y;
+if (0.5 < x < 0.5 ) { cout << "+"; }
+else { cout << "-"; }
+if (1 < y < 1) { cout << "+"; }
+else { cout << "-"; }
